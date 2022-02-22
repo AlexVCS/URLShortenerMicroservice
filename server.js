213eb214dc35284a6a804ab99fd17f396f64db3a
Alex Curtis-Slep <alexcurtisslep@Alexs-MacBook-Air.local>
2022-02-22 10:49:10 -0500

    setting up the project

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const dns = require('dns');
 const app = express();
+
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+// app.post('/api/shorturl', function(req, res) {
+
+// })
+
+// dns.lookup(host, () => {
+
+// })
 
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
