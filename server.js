045cd64fe02ad8fbde9c6a5e9a41943c086b6ae3
alexvcs <alexcurtisslep@gmail.com>
2022-08-30 16:40:28 -0400

    added options to dns.lookup function

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const router = express.Router();
 const dns = require('dns');
 const { url } = require('inspector');
 const { doesNotMatch } = require('assert');
+const { debugPort } = require('process');
 const app = express();
 const { Schema } = mongoose;
 const myURI = process.env['MONGO_URI'];
@@ -24,38 +25,58 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-mongoose.connect(`${myURI}`, {useNewUrlParser: true, useUnifiedTopology: true}, () => {
-  console.log('connected to database')
-});
-    
-const urlSchema = new Schema({
-  originalURL: String,
+mongoose.connect(`${myURI}`)
+
+const urlSchema = new mongoose.Schema({
+  original_url: String,
   short_url: Number
 })
 
-const UrlModel = mongoose.model('Url', urlSchema)
+const Url = mongoose.model('Url', urlSchema)
 
-let urlCode = Math.floor(Math.random() * 10)
+app.post('/api/shorturl', async function (req, res) {
+  const inputUrl = req.body.url
+  const options = {
+    verbatim: new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/)
+  }
+  dns.lookup('http://example.com', options, async (error) => {
+        console.log(error)
+        if (error) {
+          res.json({
+          error: 'Invalid URL'})}
+        else {
+          console.log('hi I am running')
+          const totalCount = await Url.count()
+          let urlCode = totalCount + 1
+          const newRedirect = new Url ({
+              "original_url": inputUrl,
+              "short_url": urlCode
+            })
+            const {original_url, short_url} = await newRedirect.save()
+            res.json({original_url, short_url})
+          }
+          })
 
-app.post('/api/shorturl', function (req, res) {
-  res.json({
-      "original_url": req.body.url,
-      "short_url": urlCode
-    })
 })
 
-// app.get(`/api/shorturl/${urlCode}`, function(req, res) {
-//   res.redirect(req.body.url)
-// })
 
-// dns.lookup('req.body.url', (err, data) => {
-//   if(err) {
-//     console.log(err);
-//     return
-//   } else {
-//     data.save()
-//   }
-// })
+app.get('/api/shorturl/:short_url', cors(), function(req, res) {
+  const query = parseInt(req.params.short_url)
+
+  Url.findOne({
+    short_url: query
+  }, function(err, redirectObj) {
+    if (err) throw err
+    if (redirectObj) {
+      res.redirect(redirectObj.original_url)
+    } else {
+      console.log(redirectObj)
+      res.send(JSON.stringify({
+        error: 'Invalid URL'
+      }))
+    }
+  })
+})
 
 // First API endpoint
 app.get('/api/hello', function(req, res) {
